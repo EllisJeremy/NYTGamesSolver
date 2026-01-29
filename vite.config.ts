6207ed72eec7475a3a05ac52ed6756a817478369
Jeremy Ellis <jeremyellis173@gmail.com>
2026-01-29 15:23:26 -0500

    feat: hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./public/manifest.json";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, "popup.html"),
-        background: resolve(__dirname, "src/logic/browserScope/background.ts"),
-        content: resolve(__dirname, "src/logic/pageScope/content.ts"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-      },
-    },
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  plugins: [react(), crx({ manifest })],
 });
