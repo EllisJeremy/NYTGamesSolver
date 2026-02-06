53bf174d6b990467edd0c99adcb372e099599d1e
Jeremy Ellis <jeremyellis173@gmail.com>
2026-02-06 15:05:32 -0500

    feat: slide screens

--- src/popup/components/body/Body.tsx
+++ src/popup/components/body/Body.tsx
@@ -1,13 +1,34 @@
 import SideBar from "../sideBar/SideBar";
 import styles from "./body.module.css";
 import Header from "../header/Header";
+import { usePopupStore } from "../../state/usePopupStore";
+import { AnimatePresence, motion } from "framer-motion";
+import { colors } from "../../utils/styles";
 
 export default function Body() {
+  const { currentSolver } = usePopupStore();
   return (
-    <div className={styles.container}>
+    <>
       <Header />
-      <SideBar />
-      <div className={styles.body}></div>
-    </div>
+      <div className={styles.container}>
+        <SideBar />
+        <div className={styles.body}>
+          <AnimatePresence mode="wait">
+            <motion.div
+              className={styles.tile}
+              style={{ backgroundColor: colors[currentSolver] }}
+              key={currentSolver}
+              initial={{ y: 400, scale: 0.96 }}
+              animate={{ y: 0, scale: 1 }}
+              exit={{ y: 400, scale: 0.92 }}
+              transition={{ duration: 0.3 }}
+            >
+              {currentSolver}
+              <button className={styles.solve}>Solve</button>
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      </div>
+    </>
   );
 }
