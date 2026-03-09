6c2595ae0bcbabd8079bcc8277cf10ede52b68ea
Jeremy Ellis <jeremyellis173@gmail.com>
2026-03-09 16:18:09 -0400

    feat: move pointer for prefix Tree

--- src/logic/pageScope/strands/strands.test.ts
+++ src/logic/pageScope/strands/strands.test.ts
@@ -3,8 +3,11 @@ import { PrefixTree } from "./utils/prefixTree";
 
 describe("strands", () => {
   test("prefix tree", () => {
-    const seedData = ["word1", "word2", "apple"];
+    const seedData = ["word1", "word2", "apple", "w"];
     const prefixTree = new PrefixTree(seedData);
     console.log(JSON.stringify(prefixTree));
+
+    const res = prefixTree.movePointer("w");
+    console.log(res);
   });
 });
--- src/logic/pageScope/strands/utils/prefixTree.ts
+++ src/logic/pageScope/strands/utils/prefixTree.ts
@@ -5,12 +5,14 @@ type TreeNode = {
 
 export class PrefixTree {
   root: TreeNode;
+  pointer: TreeNode;
 
   constructor(seedData: string[]) {
     this.root = {
       next: {},
       isWord: true,
     };
+    this.pointer = this.root;
 
     for (const word of seedData) {
       this.addWord(word);
@@ -26,5 +28,24 @@ export class PrefixTree {
       pointer = pointer.next[letter];
     }
     pointer.isWord = true;
+  }
+
+  newPath() {
+    this.pointer = this.root;
+  }
+
+  movePointer(letter: string): { next: string[]; isWord: boolean } {
+    if (!(letter in this.pointer.next)) {
+      throw new Error("invalid letter");
+    }
+
+    this.pointer = this.pointer.next[letter];
+    const res = {
+      next: Object.keys(this.pointer.next),
+      isWord: this.pointer.isWord,
+    };
+    // assuming that strands will only ever have 1 of each word
+    this.pointer.isWord = false;
+    return res;
   }
 }
