1f852d30b9d9ddbae6b4d884805c6ad6f7005763
Jeremy Ellis <jeremyellis173@gmail.com>
2026-03-16 18:03:17 -0400

    test: simulate wordle only in ts

---
+++ src/logic/pageScope/wordle/worlde.test.ts
@@ -0,0 +1,89 @@
+/// <reference types="jest" />
+import { WORDS } from "./data/wordleWords";
+import updateHints from "./utils/updateHints";
+import shrinkAnswerSpace from "./utils/shrinkAnswerSpace";
+import getNextGuess from "./utils/getNextGuess";
+import type {
+  LetterGuessType,
+  AccuracyEnum,
+  PresentType,
+} from "./wordleTypes/wordleTypes";
+
+function guessWordAndGetFeedback(
+  target: string,
+  guess: string,
+): LetterGuessType[] {
+  const targetCounter: Record<string, number> = {};
+  for (const letter of target) {
+    targetCounter[letter] = (targetCounter[letter] ?? 0) + 1;
+  }
+  const guessCounter: Record<string, number> = {};
+  const feedback = [];
+
+  for (let i = 0; i < 5; i++) {
+    guessCounter[guess[i]] = (guessCounter[guess[i]] ?? 0) + 1;
+
+    let accuracy: AccuracyEnum = "absent";
+    if (guess[i] === target[i]) {
+      accuracy = "correct";
+    } else if (
+      target.includes(guess[i]) &&
+      guessCounter[guess[i]] <= (targetCounter[guess[i]] ?? 0)
+    ) {
+      accuracy = "present in another position";
+    }
+    feedback.push({
+      position: i,
+      letter: guess[i],
+      accuracy,
+    });
+  }
+
+  return feedback;
+}
+
+function wordle(target: string): number {
+  const answerSpace = new Set(WORDS);
+  const absent: Set<string> = new Set();
+  const present: PresentType = {};
+  const correct = [null, null, null, null, null];
+  let guess = "crane";
+
+  for (let rowIndex = 0; rowIndex <= 5; rowIndex++) {
+    const feedback = guessWordAndGetFeedback(target, guess);
+    console.log(feedback);
+
+    updateHints(absent, present, correct, feedback);
+    if (correct.filter((x) => x === null).length === 0) {
+      return rowIndex;
+    }
+
+    shrinkAnswerSpace(absent, present, correct, answerSpace);
+    if (answerSpace.size === 0) {
+      throw new Error(`Answer space did not contain ${target}`);
+    }
+    console.log(answerSpace);
+    guess =
+      answerSpace.size <= 2
+        ? answerSpace.values().next().value!
+        : getNextGuess(answerSpace);
+  }
+  throw new Error(`${target} was not solved`);
+}
+
+describe("wordle", () => {
+  test("all words", () => {
+    let runningSum = 0;
+    for (const word of WORDS) {
+      const numberOfGuesses = wordle(word);
+      runningSum += numberOfGuesses;
+    }
+    const mean = runningSum / WORDS.length;
+    console.log(mean);
+  });
+
+  test("one word", () => {
+    const numberOfGuesses = wordle("abbed");
+    console.log(numberOfGuesses);
+  });
+});
